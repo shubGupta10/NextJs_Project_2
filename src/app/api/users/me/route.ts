e1d388855b3ca61d2bfa9a82fc2ca1f2bb8a45f5
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-09-15 17:35:39 +0530

    Profile Backend is done

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,26 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/UserModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request:NextRequest){
+
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+
+        if(!user){
+            return NextResponse.json({error: "User did not Found"}, {status: 500});
+        }
+        return NextResponse.json({
+            mesaaage: "User found",
+            data: user
+        })
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 400});
+    }
+
+}
